484649e0f4d8743f5e29f58cbdfcc96952f3a140
Broc Seigneurie <baseigneurie@gmail.com>
2018-12-06 23:36:53 -0600

    Flushed out the features

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-// const home = require('./views/index.html')
 
+app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
 
-app.listen(3000, () => {
+
+app.listen(8080, () => {
     console.log('Listening on port 3000 now!');
 });
